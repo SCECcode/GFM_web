6ee82904e85e3d833c642e74d686a99cce2a526d
Mei-Hui Su <mei@MeiPro.local>
2019-08-12 13:35:02 -0700

    tweak pwd

--- web/php/getUCVMCMaterialPropertyByLatlon.php
+++ web/php/getUCVMCMaterialPropertyByLatlon.php
@@ -10,6 +10,9 @@ $firstlon = ($_GET['firstlon']);
 $firstz = ($_GET['firstz']);
 $firstzmode = ($_GET['firstzmode']);
 
+putenv("LD_LIBRARY_PATH=../model/UCVMC_TARGET/lib/euclid3/lib:../model/UCVMC_TARGET/lib/proj-5/lib");
+putenv("DYLD_LIBRARY_PATH=../model/UCVMC_TARGET/lib/euclid3/lib:../model/UCVMC_TARGET/lib/proj-5/lib");
+
 $estr = " -l ".$firstlat.",".$firstlon.",".$firstz." ";
 
 $query="../model/UCVMC_TARGET/bin/ucvm_query -m cvmh -f ../model/UCVMC_TARGET/conf/ucvm.conf -c gd -b ".$estr;
@@ -17,7 +20,12 @@ $query="../model/UCVMC_TARGET/bin/ucvm_query -m cvmh -f ../model/UCVMC_TARGET/co
 if ($firstzmode == 'e') 
      $query="../model/UCVMC_TARGET/bin/ucvm_query -m cvmh -f ../model/UCVMC_TARGET/conf/ucvm.conf -c ge -b ".$estr;
 
+
+echo $query;
+
 $result = exec(escapeshellcmd($query), $retval);
+
+echo $result;
 
 $resultstring = htmlspecialchars($result, ENT_QUOTES, 'UTF-8');
 echo "<div data-side=\"materialPropertyUCVMCByLatlon\" data-params=\""; 
