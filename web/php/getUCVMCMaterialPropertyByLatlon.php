c6dbfa1ab49c65542d84e1ab4e7cdb1566666a07
root <root@ip-172-31-28-24.us-west-2.compute.internal>
2019-08-13 03:57:28 +0000

    test

--- web/php/getUCVMCMaterialPropertyByLatlon.php
+++ web/php/getUCVMCMaterialPropertyByLatlon.php
@@ -10,8 +10,10 @@ $firstlon = ($_GET['firstlon']);
 $firstz = ($_GET['firstz']);
 $firstzmode = ($_GET['firstzmode']);
 
-putenv("LD_LIBRARY_PATH=../model/UCVMC_TARGET/lib/euclid3/lib:../model/UCVMC_TARGET/lib/proj-5/lib");
-putenv("DYLD_LIBRARY_PATH=../model/UCVMC_TARGET/lib/euclid3/lib:../model/UCVMC_TARGET/lib/proj-5/lib");
+
+putenv("LD_LIBRARY_PATH=/var/www/html/GFM/web/model/UCVMC_TARGET/lib/euclid3/lib:/var/www/html/GFM/web/model/UCVMC_TARGET/lib/proj-5/lib");
+putenv("DYLD_LIBRARY_PATH=/var/www/html/GFM/web/model/UCVMC_TARGET/lib/euclid3/lib:/var/www/html/GFM/web/model/UCVMC_TARGET/lib/proj-5/lib"); 
+
 
 $estr = " -l ".$firstlat.",".$firstlon.",".$firstz." ";
 
@@ -20,12 +22,7 @@ $query="../model/UCVMC_TARGET/bin/ucvm_query -m cvmh -f ../model/UCVMC_TARGET/co
 if ($firstzmode == 'e') 
      $query="../model/UCVMC_TARGET/bin/ucvm_query -m cvmh -f ../model/UCVMC_TARGET/conf/ucvm.conf -c ge -b ".$estr;
 
-
-echo $query;
-
 $result = exec(escapeshellcmd($query), $retval);
-
-echo $result;
 
 $resultstring = htmlspecialchars($result, ENT_QUOTES, 'UTF-8');
 echo "<div data-side=\"materialPropertyUCVMCByLatlon\" data-params=\""; 
