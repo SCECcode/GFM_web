98ae2e72c00c81fc92bc27ef15832354549fb128
Mei-Hui Su <mei@MeiPro.local>
2019-08-20 21:12:01 -0700

    mac version

--- web/php/util.php
+++ web/php/util.php
@@ -2,7 +2,7 @@
 function makeEnvString() {
    $myhost = gethostname();
    if ($myhost == "MeiPro.local") {
-      $envstr="PROJ_LIB=/Users/mei/anaconda2/pkgs/proj4-5.2.0-h0a44026_1/share/proj PATH=../model/UCVMC_TARGET/bin:/Users/mei/SCEC/anaconda2/bin:/usr/local/opt/libxml2/bin:/usr/local/opt/sqlite/bin:/usr/local/opt/libxml2/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin PYTHONPATH=../model/UCVMC_TARGET/utilities/pycvm";
+      $envstr="PROJ_LIB=/Users/mei/anaconda2/share/proj PATH=../model/UCVMC_TARGET/bin:/Users/mei/SCEC/anaconda2/bin:/usr/local/opt/libxml2/bin:/usr/local/opt/sqlite/bin:/usr/local/opt/libxml2/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin PYTHONPATH=../model/UCVMC_TARGET/utilities/pycvm";
    } 
 
    return $envstr;
